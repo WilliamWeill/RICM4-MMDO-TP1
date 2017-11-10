99aa6736e11ef589bedf596650a154781cd67e4a
William Weill <wweill84@gitlab.com>
2017-11-10 16:26:08 +0100

    Step 9A - interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+interface results {
+  title: string,
+  author: string,
+  title: string,
+  image: string
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
